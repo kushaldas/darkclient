95df9e3ad0cacd779b64cf6279002cd6dd366531
Kushal Das <kdas@redhat.com>
2011-11-11 16:17:07 +0530

    Initial code import.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""darkclient command line tool"""
+from distutils.core import setup
+
+
+setup(name='darkclient',
+      version='0.1',
+      description="command line tool for darkserver",
+      long_description = "command line tool for darkserver",
+      platforms = ["Linux"],
+      author="Kushal Das",
+      author_email="kdas@redhat.com",
+      url="https://github.com/kushaldas/darkclient",
+      license = "http://www.gnu.org/copyleft/gpl.html",
+      data_files=[("/usr/bin",['darkclient']),
+          ('/usr/share/darkclient', ['README', 'LICENSE'])]
+      )
