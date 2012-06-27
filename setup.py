1021d2fd6316bd1ab7e731033a371d96942f8cf3
Kushal Das <kushal@fedoraproject.org>
2012-06-27 18:41:41 +0530

    Updated version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 
 setup(name='darkclient',
-      version='0.1',
+      version='0.2',
       description="Command line tool for darkserver",
       long_description = "Command line tool for darkserver",
       platforms = ["Linux"],
